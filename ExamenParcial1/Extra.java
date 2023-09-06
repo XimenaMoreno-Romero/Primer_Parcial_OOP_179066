cfac536376866ddf1efc94f5e367d96bfa02cebd
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-05 23:15:04 -0600

    Create Extra.java

---
+++ ExamenParcial1/Extra.java
@@ -0,0 +1,20 @@
+package examen;
+public class Extra {
+	public void ejecutaContadordeFizzbzzparamultiplosde3y5() {
+		int end = 100;
+		for (int i = 1; i <= end; i++) {
+		        if (((i % 3) == 0) && ((i % 5) == 0)) 
+		        {
+		         System.out.println("FizzBuzz");
+		        } else if ((i % 3) == 0) 
+		        {
+		          System.out.println("Fizz");
+		        } else if ((i % 5) == 0) 
+		        {
+		         System.out.println("Buzz");
+		        } else {
+		          System.out.println(i); 
+		        }
+	}
+	}
+}
