819cc8d8c6a475f74a7472e990a91f3e8eec06e2
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-13 10:51:06 -0600

    Create MyStack.java

---
+++ Actividad_4/MyStack.java
@@ -0,0 +1,34 @@
+package programa;
+
+public class MyStack {
+	int MAX, top ;
+	int[] stack;
+public MyStack(int size ) {
+	this.top=0;
+	this.MAX = size;
+	this.stack= new int[size];
+}
+	 public void push(int data)
+	 {
+	 if(top == MAX)
+	 System.out.println("Stack is full");
+	 else {
+	 stack[top] = data; /* agrega un elemento*/
+	 top++; /* incrementa el valor de top */
+	 }
+	 }
+	 
+	 public int pop()
+	 {
+	 if(top ==0 )
+	 {
+	 System.out.println(" Stack empty");
+	 return ( top);
+	 }
+	 else {
+	 top; /* decreases top */
+	 return ( MAX); /*returns eliminated element*/
+	 }
+	 }
+
+ }
