6fb5633c6045e9c69831d298b803132772f09095
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-13 10:50:34 -0600

    Create Ejecutador.java

---
+++ Actividad_4/Ejecutador.java
@@ -0,0 +1,10 @@
+package programa;
+
+public class Ejecutador {
+	public void iniciarPrograma() {
+		MyQueue newQueue= new MyQueue();
+		MyStack newStack= new MyStack(15);
+		newStack.push(2);
+	}
+
+}
