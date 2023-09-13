5cb9a563c70b20cbf89851c1ca47e7a65c0d205b
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-13 10:52:07 -0600

    Create MyQueue.java

---
+++ Actividad_4/MyQueue.java
@@ -0,0 +1,13 @@
+package programa;
+
+public class MyQueue {
+	public void insert (int data) {
+	int tail, MAX;
+	{
+	if(tail == MAX)
+	System.out.println("Queue is full");
+	else {
+	queue[tail] = data;/* inserts data*/
+	tail++; /* increases index*/
+	}
+	}}}
