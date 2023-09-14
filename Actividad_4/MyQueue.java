cc2d37511b6c62c45b40b4af8c0977435ecc6146
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-14 14:14:19 -0600

    Update MyQueue.java

--- Actividad_4/MyQueue.java
+++ Actividad_4/MyQueue.java
@@ -1,13 +1,18 @@
 package programa;
 
-public class MyQueue {
-	public void insert (int data) {
-	int tail, MAX;
-	{
-	if(tail == MAX)
-	System.out.println("Queue is full");
-	else {
-	queue[tail] = data;/* inserts data*/
-	tail++; /* increases index*/
+public class MyQueues {
+	int MAX, tail ;
+	int[] queue;
+	public MyQueues(int size ) {
+		this.tail=0;
+		this.MAX = size;
+		this.queue= new int[size];
 	}
-	}}}
+	public void insert (int data) {
+		if(tail == MAX) {
+		System.out.println("Queue is full");}
+		else {
+		queue[tail] = data;/* inserts data*/
+		tail++; /* increases index*/
+		}
+		}}
