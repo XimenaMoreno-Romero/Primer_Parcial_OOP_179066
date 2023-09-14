f1a7af5f5afbb30f3c4c528f7f54223e725a1afc
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-14 14:15:18 -0600

    Update Ejecutador.java

--- Actividad_4/Ejecutador.java
+++ Actividad_4/Ejecutador.java
@@ -2,9 +2,28 @@ package programa;
 
 public class Ejecutador {
 	public void iniciarPrograma() {
-		MyQueue newQueue= new MyQueue();
-		MyStack newStack= new MyStack(15);
-		newStack.push(2);
+		MyStack newStack= new MyStack(8);
+		newStack.push(30);
+		newStack.push(30);
+		newStack.push(30);
+		newStack.push(30);
+		newStack.push(30);
+		newStack.push(30);
+		newStack.push(30);
+		newStack.push(30);
+		newStack.push(30);
+		
+		MyQueues newQueue= new MyQueues(8);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		newQueue.insert(30);
+		
 	}
 
 }
