aaa9ac6c771a0740ea5af8cc504273035b3e6673
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-14 21:20:49 -0600

    Update Ejecutador.java

--- Actividad_4/Ejecutador.java
+++ Actividad_4/Ejecutador.java
@@ -1,29 +1,42 @@
-package programa;
+package actividad4;
 
 public class Ejecutador {
 	public void iniciarPrograma() {
 		MyStack newStack= new MyStack(8);
+		MyQueues newQueue= new MyQueues(5);
+		newStack.push(90);
+		newStack.push(80);
+		newStack.push(70);
+		newStack.print();
+
+		newStack.push(60);
+		newStack.push(50);
+		newStack.push(40);
 		newStack.push(30);
-		newStack.push(30);
-		newStack.push(30);
-		newStack.push(30);
-		newStack.push(30);
-		newStack.push(30);
-		newStack.push(30);
-		newStack.push(30);
-		newStack.push(30);
+		newStack.push(20);
+		newStack.pop();
+		newStack.print();
 		
-		MyQueues newQueue= new MyQueues(8);
-		newQueue.insert(30);
-		newQueue.insert(30);
-		newQueue.insert(30);
-		newQueue.insert(30);
-		newQueue.insert(30);
-		newQueue.insert(30);
-		newQueue.insert(30);
-		newQueue.insert(30);
-		newQueue.insert(30);
+		//System.out.println();
+		//int x = newStack.pop();
+		//System.out.println(x);
+		newStack.pop();
+		newStack.pop();
+		newStack.pop();
+		newStack.pop();
+		newStack.pop();
+		newStack.pop();
+		newStack.pop();
 		
+	
+		newQueue.insert(6);
+		newQueue.insert(3);
+		newQueue.insert(4);
+		newQueue.insert(3);
+		newQueue.insert(2);
+		newQueue.delete();
+		newQueue.delete();	
+		newQueue.delete();		
 	}
 
 }
