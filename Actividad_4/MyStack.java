277a78884877e7c22ea7031456b316d8cec34c0b
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-14 21:22:03 -0600

    Update MyStack.java

--- Actividad_4/MyStack.java
+++ Actividad_4/MyStack.java
@@ -1,35 +1,43 @@
-package programa;
+package actividad4;
 
 public class MyStack {
 	
-		int MAX, top ;
-		int[] stack;
-	public MyStack(int size ) {
-		this.top=0;
-		this.MAX = size;
-		this.stack= new int[size];
-	}
-		 public void push(int size)
-		 {
-		 if(top == MAX)
-		 System.out.println("Stack is full");
-		 else {
-		 stack[top] = size; /* agrega un elemento*/
-		 top++; /* incrementa el valor de top */
-		 }}}
-	 }
-	 
-	 public int pop()
-	 {
-	 if(top ==0 )
-	 {
-	 System.out.println(" Stack empty");
-	 return ( top);
-	 }
-	 else {
-	 top; /* decreases top */
-	 return ( MAX); /*returns eliminated element*/
-	 }
-	 }
+	int MAX, top ;
+	int[] stack;
 
+	public MyStack(int size ) {
+	this.top=0;
+	this.MAX = size;
+	this.stack= new int[size];
+	}
+	
+	
+ public void push(int value){
+	if(top == MAX)
+		System.out.println("Stack is full");
+	else {
+		stack[top] = value; /* agrega un elemento*/
+		top++; /* incrementa el valor de top */
+	}
  }
+ public int pop() {
+	 int value;
+	if (top == 0) {
+		System.out.println("Stack is empty");
+		return(0);
+	}
+	else {
+		value= stack[top-1];
+		top--; /*decreases top*/
+		stack[top]=0;
+		return(value);/*returns eliminated element*/
+	}
+ }
+ public void print() {
+	 for (int i=0;i<top;i++) {
+		 System.out.println("stack "+stack[i]);
+	 }
+ }
+}
+
+
