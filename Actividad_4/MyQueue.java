36c9fde18aba2e09de70ba295ee421d9caf75c3c
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-15 22:50:13 -0600

    Update MyQueue.java

--- Actividad_4/MyQueue.java
+++ Actividad_4/MyQueue.java
@@ -1,42 +1,36 @@
-package actividad4;
+package ACT4;
 
-public class MyQueues {
-	int MAX, tail,head ;
-	int[] queue;
-	public MyQueues(int size ) {
-		this.tail=0;
-		this.MAX = size;
-		this.queue= new int[MAX];
-	}
-	public void insert (int data) {
-		if(tail == MAX) {
-			System.out.println("Queue is full");
-		}
-		else {
-			queue[tail] = data;/* inserts data*/
-			tail++; /* increases index*/
-		}
-	}
-	  public int delete(){
-		  int i;
-	        if (tail > 0){
-	        	head= queue[0];
-	        for ( i=0;i<tail-1;i++);{
-	        	queue[i]=queue[i+1];
-	        	
-	        }
-	        tail--;
-	        return head;
-	        	
-	        }
-	        else {
-	        	  System.out.println("#");
-	        }
-			return '#';
-	            
-	    }
+public class MyQueue {
 	
-	}
+	    int[] queue;
+	    int MAX;
+	    int tail;
+	    int Head;
+	    public MyQueue(int size){
+	        this.queue = new int[size];
+	        this.MAX = size;
+	        this.tail = 0;
+	    }
+	    public void insert (int data){
+	        if(tail == MAX)
+	            System.out.println("Queue is full");
+	        else {queue[tail] = data;/* inserts data*/
+	            tail++; /* increases index*/}
+	    }
+	    public int delete(){
+	        if (tail != 0){
+	            Head = queue[0];
+	            for (int i = 0; i < queue[tail-1]; i++) {
+	                queue[i] = queue[i+1];
+	            }
+	            tail = tail - 1;
+	            return Head;
+	        }else {
+	        	 System.out.println("#");
+	            return '#';
+	        }
+	    }
+}
 
 
 
