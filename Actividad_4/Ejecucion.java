6a379e137be9087400424a9affcc0fa862a20fad
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-16 12:51:26 -0600

    Update Ejecucion.java

--- Actividad_4/Ejecucion.java
+++ Actividad_4/Ejecucion.java
@@ -3,24 +3,19 @@ import java.util.Scanner;
 public class Ejecucion {
 	
 	public void iniciarPrograma() {
-		String cadena, cadenayaInvertida = "";
-		MyStrack newStack= new MyStrack(4);
-		MyQueue newQueue= new MyQueue(5);
-		System.out.println("Bienvenido al programa de Stacks y Queues");
-		System.out.println("Escriba una frase");
-		Scanner input = new Scanner(System.in);
-        cadena = input.nextLine();
-        MyStrack stackparaInvertir = new MyStrack(cadena.length());
-        MyQueue queue1 = new MyQueue(cadena.length());
-        cadenayaInvertida = newStack.invertirLaCadena(cadena, stackparaInvertir);
+		 System.out.print("Bienvenido al programa  Stakc y Queue ");
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Escribe una frase:");
+        MyStrack stacktest=new MyStrack(5);
+		MyQueue queuetest= new MyQueue(5);
+        String frase = scanner.nextLine();
+        scanner.close();
+        stacktest.invertirLaCadena(frase);
         
-        System.out.println(cadenayaInvertida);
-        if (newStack.verificacionPalindromo(cadena)){
-            System.out.println("La frase es un palindromo");
-        }else {
-            System.out.println("La frase no es un palindromo");
-        }
-       
+		stacktest.VerificarPalindromo(frase);     
+		queuetest.insert(12);
+		stacktest.push('a');
+		stacktest.pop();     
 		
 		
 	}
