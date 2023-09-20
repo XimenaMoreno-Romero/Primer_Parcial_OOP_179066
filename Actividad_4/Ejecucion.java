2ac31e5335602c7d0b7ebe0cd0f0da999da3934d
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-09-20 09:34:43 -0600

    Update Ejecucion.java

--- Actividad_4/Ejecucion.java
+++ Actividad_4/Ejecucion.java
@@ -1,23 +1,24 @@
-package ACT4;
+package programa;
 import java.util.Scanner;
-public class Ejecucion {
-	
-	public void iniciarPrograma() {
-		 System.out.print("Bienvenido al programa  Stakc y Queue ");
-		Scanner scanner = new Scanner(System.in);
-        System.out.print("Escribe una frase:");
-        MyStack stacktest=new MyStack(5);
-		MyQueue queuetest= new MyQueue(5);
-        String frase = scanner.nextLine();
-        scanner.close();
-        stacktest.invertirLaCadena(frase);
-        
-		stacktest.VerificarPalindromo(frase);     
-		queuetest.insert(12);
-		stacktest.push('a');
-		stacktest.pop();     
+public class Ejecutador {
+	public static void ejecucionPrograma(){	
+		MyStack stackLlamado = new MyStack(0);
+		Scanner recibidorFrase = new Scanner(System.in);
+		String fraseRecibida = "";
+		String fraseInversa = "";
 		
+		System.out.println("Bienvenido al programa de la Act # 4");
+		System.out.println("Ingresa una frase: ");
+		fraseRecibida = recibidorFrase.nextLine();
+		MyStack stacks = new MyStack(fraseRecibida.length());
+		MyQueue queues = new MyQueue(fraseRecibida.length());
+		fraseInversa = stackLlamado.inversorDelaFase (fraseRecibida, stacks);
+		System.out.println("La frase invertida es: "+ fraseInversa);
 		
+		if(stackLlamado.verificarPalindromo (fraseRecibida)){
+			System.out.println("Es palíndromo");
+		}
+		else {
+			System.out.println("No es palíndromo");
+		}
 	}
-}
-
