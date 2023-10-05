82c6d0a40b1d63e6e09e10d913a8ae412ef4daca
XimenaMoreno-Romero <141852039+XimenaMoreno-Romero@users.noreply.github.com>
2023-10-04 21:33:41 -0600

    Create Pokemon.java

---
+++ Examen Parcial 2/Pokemon.java
@@ -0,0 +1,18 @@
+public abstract class Pokemon {
+    private String nombrePokemon, tipoPokemon, poder1, poder2, poder3, poder4;
+
+    public void setNombrePokemon(String nombrePokemon) {
+        this.nombrePokemon = nombrePokemon;
+    }
+
+    public void setTipoPokemon(String tipoPokemon) {
+        this.tipoPokemon = tipoPokemon;
+    }
+
+    public void setPoder1(String poder1) {
+        this.poder1 = poder1;
+    }
+
+    public void setPoder2(String poder2) {
+        this.poder2 = poder2;
+    }
